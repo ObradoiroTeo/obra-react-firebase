4940c1b2675a7c55812f89d5fc6d8c2f71340def
SaraMiri <44493924+SaraMiri@users.noreply.github.com>
2018-12-05 11:18:53 +0100

    Creadas todas las rutas necesarias para la obra
    * Añadido `@reach-router`
    * Creadas todas las rutas necesarias
    * Falta resolver el problema de `NotFound`

---
+++ src/components/Cultura.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Cultura extends React.Component {
+  render() {
+    return <h1>Cultura</h1>;
+  }
+}
+
+export default Cultura;
---
+++ src/components/Routes.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Router } from "@reach/router";
+
+import App from "./App";
+import Home from "./Home";
+import Cultura from "./Cultura";
+import Deporte from "./Deporte";
+import Avisos from "./Avisos";
+import InfoTeo from "./InfoTeo";
+import Admin from "./Admin";
+import Salvapantallas from "./Salvapantallas";
+
+const Routes = () => (
+  <Router>
+    <App path="/">
+      <Home path="/" />
+      <Cultura path="/cultura" />
+      <Deporte path="/deporte" />
+      <Avisos path="/avisos" />
+      <InfoTeo path="/infoteo" />
+      <Admin path="/admin" />
+      <Salvapantallas path="/salvapantallas" />
+    </App>
+  </Router>
+);
+
+export default Routes;
