2211d5908fdcec686b8a38fc75f8b8ccde99ad84
davidgchaves <davidgchaves@users.noreply.github.com>
2018-12-10 14:36:45 +0100

    Fix problem with Routes (#3)
    - Merge `<Routes>` with `<App>` component into `<App>`
    - `state` and routes live on `<App>`

--- src/components/Deporte.js
+++ src/components/Deporte.js
@@ -1,8 +1,20 @@
 import React from "react";
 
+import PropTypes from "prop-types";
+
 class Deporte extends React.Component {
+  static propTypes = {
+    sportEvents: PropTypes.shape({
+      title: PropTypes.string.isRequired
+    }).isRequired
+  };
+
   render() {
-    return <h1>Deporte</h1>;
+    return (
+      <div>
+        <h2>{this.props.sportEvents.title}</h2>
+      </div>
+    );
   }
 }
 
