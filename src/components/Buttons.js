329f2cfb854b53fc8d4f5617485b3655254496c1
Iago C <iagocrpi@gmail.com>
2018-12-10 11:05:36 +0100

    Añadido el componente <Buttons>

---
+++ src/components/Buttons.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Buttons extends React.Component {
+  render() {
+    return (
+      <div>
+        <button />
+        <button />
+        <button />
+      </div>
+    );
+  }
+}
+
+export default Buttons;
