62a1df925161e0629fca129704f3d1ad59bdc621
David G Chaves <davidgchaves@gmail.com>
2018-12-10 13:53:04 +0100

    Fix problem with Routes
    - Merge <Routes> with <App> component into <App>
    - `state` and routes live on <App>

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,32 @@
 import React from "react";
+import { Router } from "@reach/router";
+
+import Home from "./Home";
+import Cultura from "./Cultura";
+import Deporte from "./Deporte";
+import Avisos from "./Avisos";
+import InfoTeo from "./InfoTeo";
+import Admin from "./Admin";
+import Salvapantallas from "./Salvapantallas";
 
 class App extends React.Component {
+  state = {
+    deporte: { title: "🚀🤖👾 Deporte Testing (CHANGE ME)!" }
+  };
+
   render() {
     return (
       <>
         <h1>App</h1>
-        {this.props.children}
+        <Router>
+          <Home path="/" />
+          <Cultura path="/cultura" />
+          <Deporte path="/deporte" sportEvents={this.state.deporte} />
+          <Avisos path="/avisos" />
+          <InfoTeo path="/infoteo" />
+          <Admin path="/admin" />
+          <Salvapantallas path="/salvapantallas" />
+        </Router>
       </>
     );
   }
