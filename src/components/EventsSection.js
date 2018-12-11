4fd84de20dcf4e2088071a534597393eba37440f
Iago C <iagocrpi@gmail.com>
2018-12-11 08:29:17 +0100

    Muestra los 4 eventos

--- src/components/EventsSection.js
+++ src/components/EventsSection.js
@@ -1,13 +1,18 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import Evento from "./Evento";
 
 class EventsSection extends React.Component {
+  static propTypes = {
+    eventosDetails: PropTypes.string.isRequired
+  };
+
   render() {
     return (
       <ul className="eventos">
         {Object.keys(this.props.events).map(eventKey => (
-          <Evento key={eventKey} events={this.props.events} />
+          <Evento key={eventKey} eventDetails={this.props.events[eventKey]} />
         ))}
       </ul>
     );
