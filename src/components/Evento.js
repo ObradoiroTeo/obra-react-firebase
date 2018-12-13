902a629964ab2f8c68affbd3d1f44bc75d89ecbd
Sara Miragaya Rico <saramiragaya@gmail.com>
2018-12-13 10:38:17 +0100

    Transformados `Evento` y `SectionEvents` en functional components

--- src/components/Evento.js
+++ src/components/Evento.js
@@ -1,24 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-class Evento extends React.Component {
-  static propTypes = {
-    eventDetails: PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      date: PropTypes.string.isRequired,
-      desc: PropTypes.string.isRequired
-    }).isRequired
-  };
+const Evento = props => (
+  <div>
+    <p>{this.props.eventDetails.name}</p>
+    <img src="" alt="" />
+    <p>{this.props.eventDetails.date}</p>
+    <p>{this.props.eventDetails.desc}</p>
+  </div>
+);
 
-  render() {
-    return (
-      <div>
-        <p>{this.props.eventDetails.name}</p>
-        <img src="" alt="" />
-        <p>{this.props.eventDetails.date}</p>
-        <p>{this.props.eventDetails.desc}</p>
-      </div>
-    );
-  }
-}
+Evento.propTypes = {
+  eventDetails: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    desc: PropTypes.string.isRequired
+  }).isRequired
+};
+
 export default Evento;
--- src/components/SectionEvents.js
+++ src/components/SectionEvents.js
@@ -1,20 +1,18 @@
 import React from "react";
 import Evento from "./Evento";
 import PropTypes from "prop-types";
-class SectionEvents extends React.Component {
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    events: PropTypes.object.isRequired
-  };
 
-  render() {
-    return (
-      <ul className="events">
-        {Object.keys(this.props.events).map(eventKey => (
-          <Evento key={eventKey} eventDetails={this.props.events[eventKey]} />
-        ))}
-      </ul>
-    );
-  }
-}
+const SectionEvents = props => (
+  <ul className="events">
+    {Object.keys(this.props.events).map(eventKey => (
+      <Evento key={eventKey} eventDetails={this.props.events[eventKey]} />
+    ))}
+  </ul>
+);
+
+SectionEvents.propTypes = {
+  title: PropTypes.string.isRequired,
+  events: PropTypes.object.isRequired
+};
+
 export default SectionEvents;
