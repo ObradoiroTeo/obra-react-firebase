e521ff12b4fadb638c3f9813377c574368979702
Iago C <iagocrpi@gmail.com>
2018-12-13 09:32:26 +0100

    Creado el componente <SectionTitle>

--- src/components/Cultura.js
+++ src/components/Cultura.js
@@ -2,7 +2,7 @@ import React from "react";
 
 class Cultura extends React.Component {
   render() {
-    return <h1>Cultura</h1>;
+    return <div />;
   }
 }
 
---
+++ src/components/SectionTitle.js
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+class SectionTitle extends React.Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+      </div>
+    );
+  }
+}
+
+export default SectionTitle;
