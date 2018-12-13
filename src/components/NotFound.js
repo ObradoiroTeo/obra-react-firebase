35678dd3970b616b80f3d60fee61898b04fbdc7c
Sara Miragaya Rico <saramiragaya@gmail.com>
2018-12-13 14:05:14 +0100

    Transformado `NotFound` en un functional component

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,8 +1,5 @@
 import React from "react";
 
-class NotFound extends React.Component {
-  render() {
-    return <h2>Erro 404! Páxina non atopada!</h2>;
-  }
-}
+const NotFound = props => <h2>Erro 404! Páxina non atopada!</h2>;
+
 export default NotFound;
