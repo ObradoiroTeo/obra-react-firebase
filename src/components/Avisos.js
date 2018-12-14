53f1708e59d6fba79fe28e3c8dd8cfdbe9b594d7
Sara Rodríguez Moldes <raheld12@gmail.com>
2018-12-14 13:53:15 +0100

    Añadido componente Header al resto de componentes

--- src/components/Avisos.js
+++ src/components/Avisos.js
@@ -1,4 +1,6 @@
 import React from "react";
+
+import Header from "./Header";
 import SectionTitle from "./SectionTitle";
 import SectionEvents from "./SectionEvents";
 
@@ -6,6 +8,7 @@ class Avisos extends React.Component {
   render() {
     return (
       <div>
+        <Header />
         <SectionTitle title={this.props.notifications.title} />
         <SectionEvents events={this.props.notifications.events} />
       </div>
--- src/components/Cultura.js
+++ src/components/Cultura.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import Header from "./Header";
 import SectionTitle from "./SectionTitle";
 import Buttons from "./Buttons";
 import SectionEvents from "./SectionEvents";
@@ -8,6 +9,7 @@ class Cultura extends React.Component {
   render() {
     return (
       <div>
+        <Header />
         <SectionTitle title={this.props.cultureEvents.title} />
         <Buttons />
         <SectionEvents events={this.props.cultureEvents.events} />
