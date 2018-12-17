5c9a422d996f1e47ea980d3bc29739293c38a4bf
iagocrespo <44493921+iagocrespo@users.noreply.github.com>
2018-12-17 09:05:23 +0100

    Filtrado de eventos pasados en `<SectionEvents>` (#21)
    * Modificado el `state` añadiendo el campo `visible`
    * Añadido el filtro de eventos pasados dentro de `<SectionEvents>`

--- src/components/SectionEvents.js
+++ src/components/SectionEvents.js
@@ -4,9 +4,16 @@ import PropTypes from "prop-types";
 
 const SectionEvents = props => (
   <ul className="events">
-    {Object.keys(props.events).map(eventKey => (
-      <Evento key={eventKey} eventDetails={props.events[eventKey]} />
-    ))}
+    {Object.keys(props.events)
+      .filter(eventKey => {
+        const actualDate = new Date();
+        const eventDate = new Date(props.events[eventKey].date);
+        return actualDate < eventDate;
+      })
+      .filter(eventKey => props.events[eventKey].visible)
+      .map(eventKey => (
+        <Evento key={eventKey} eventDetails={props.events[eventKey]} />
+      ))}
   </ul>
 );
 
