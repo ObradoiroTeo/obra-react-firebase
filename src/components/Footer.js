52e88f3b9f5f69f3e55556291da7bd0f4fe5cb21
Alex Ramos <alex@example.com>
2018-12-20 11:01:35 +0100

    Arreglado el problema con las rutas del `<Header>`, `<Footer>`

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,30 @@
 import React from "react";
-import concelloTeo from "../images/logo_pequeno.png";
-import concelloStgo from "../images/concello-santiago.jpg";
-import europa from "../images/union-europea.jpg";
-import xunta from "../images/xunta-galicia.jpg";
 
 class Footer extends React.Component {
   render() {
     return (
       <span className="footer-container">
-        <img src={europa} className="logo-ue" alt="logo-ue" />
-        <img src={concelloStgo} className="logo-stgo" alt="logo-stgo" />
-        <img src={concelloTeo} className="logo-teo" alt="logo-teo" />
-        <img src={xunta} className="logo-xunta" alt="logo-xunta" />
+        <img
+          src={"/images/union-europea.jpg"}
+          className="logo-ue"
+          alt="logo-ue"
+        />
+        <img
+          src={"/images/concello-santiago.jpg"}
+          className="logo-stgo"
+          alt="logo-stgo"
+        />
+        <img
+          src={"/images/logo_pequeno.png"}
+          className="logo-teo"
+          alt="logo-teo"
+        />
+
+        <img
+          src={"/images/xunta-galicia.jpg"}
+          className="logo-xunta"
+          alt="logo-xunta"
+        />
       </span>
     );
   }
