663b5d5d986166791115bafe43f0d87b072f7640
Sara Rodríguez Moldes <raheld12@gmail.com>
2018-12-20 10:12:13 +0100

    Añadido CSS a <EventDetail>

--- src/components/EventDetails.js
+++ src/components/EventDetails.js
@@ -1,14 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Header from "./Header";
+import Footer from "./Footer";
+import magosto from "../images/magosto.jpg";
 
 const EventDetails = props => (
   <div>
     <Header />
-    <p>{props.eventDetails.name}</p>
-    <img src="" alt="" />
-    <p>{props.eventDetails.date}</p>
-    <p>{props.eventDetails.desc}</p>
+    <div className="event-container">
+      <p className="details-name">{props.eventDetails.name}</p>
+      <p className="details-date">{props.eventDetails.date}</p>
+      <div className="details-container">
+        <img src={magosto} alt="img" className="details-img" />
+        <p className="details-desc">{props.eventDetails.desc}</p>
+      </div>
+    </div>
+    <div className="empty" />
+    <Footer />
   </div>
 );
 
