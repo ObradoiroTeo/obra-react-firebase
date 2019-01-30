d87eb6c06a3a92dd4835cb9ddbd6061a11ac12b6
Iago C <iagocrpi@gmail.com>
2019-01-30 10:51:23 +0100

    Movimos los datos del state a 3 nuevos componentes <sample-avisos> <sample-deporte> y <sample-cultura> y modificamos <App> para poder conectarnos con Firebase

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,8 +1,22 @@
 import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
 
 class Admin extends React.Component {
+  cargarArchivosDeporte = event => {};
+
   render() {
-    return <h1>Admin</h1>;
+    return (
+      <h1>
+        <Header />
+        <button onClick={this.props.Sport}>Cargar archivos de Deporte</button>
+        <button onClick={this.props.Culture}>Cargar archivos de Cultura</button>
+        <button onClick={this.props.Avisos}>
+          Cargar archivos de Avisos y Notificaciones
+        </button>
+        <Footer />
+      </h1>
+    );
   }
 }
 
