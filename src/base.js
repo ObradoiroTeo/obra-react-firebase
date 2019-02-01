c362c8818faae2008ff34943dd2a5640d0bb037c
iagocrespo <44493921+iagocrespo@users.noreply.github.com>
2019-02-01 08:17:10 +0100

    Solucionado el problema de ayer al conectar la App a Firebase (#46)

---
+++ src/base.js
@@ -0,0 +1,14 @@
+import Rebase from "re-base";
+import firebase from "firebase/app";
+import "firebase/database";
+
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL
+};
+const firebaseApp = firebase.initializeApp(config);
+const base = Rebase.createClass(firebaseApp.database());
+
+export { firebaseApp };
+export default base;
--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,8 +1,24 @@
 import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
 
 class Admin extends React.Component {
   render() {
-    return <h1>Admin</h1>;
+    return (
+      <div>
+        <Header />
+        <button className="fichero-deporte" onClick={this.props.Sport}>
+          Cargar fichero deporte
+        </button>
+        <button className="fichero-cultura" onClick={this.props.Culture}>
+          Cargar fichero cultura
+        </button>
+        <button className="fichero-avisos" onClick={this.props.Avisos}>
+          Cargar fichero avisos
+        </button>
+        <Footer />
+      </div>
+    );
   }
 }
 
