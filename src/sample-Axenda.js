33248b98f8e1f002f6cbcc515fbabc88a9a8be42
Alex Ramos <alex@example.com>
2019-02-21 11:27:25 +0100

    Actualizada las fechas de los eventos

--- src/sample-Axenda.js
+++ src/sample-Axenda.js
@@ -4,7 +4,7 @@ const axenda = {
     event1: {
       name: "cultureEvento 1",
       image: "/images/cultureEvent1.jpg",
-      date: "2018-12-15",
+      date: "2018-01-15",
       visible: true,
       desc:
         "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, excepturi?",
@@ -14,7 +14,7 @@ const axenda = {
     event2: {
       name: "cultureEvento 2",
       image: "/images/cultureEvent2.jpg",
-      date: "2019-01-22",
+      date: "2019-02-22",
       visible: true,
       desc:
         "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, excepturi?",
@@ -24,7 +24,7 @@ const axenda = {
     event3: {
       name: "cultureEvento 3",
       image: "/images/cultureEvent1.jpg",
-      date: "2019-02-29",
+      date: "2019-03-29",
       visible: true,
       desc:
         "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, excepturi?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, excepturi?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, excepturi?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, excepturi?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, excepturi?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, excepturi?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, excepturi?",
@@ -34,7 +34,7 @@ const axenda = {
     event4: {
       name: "cultureEvento 4",
       image: "/images/cultureEvent4.jpg",
-      date: "2019-02-20",
+      date: "2019-03-20",
       visible: true,
       desc:
         "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, excepturi?",
@@ -44,7 +44,7 @@ const axenda = {
     event5: {
       name: "cultureEvento 5",
       image: "/images/cultureEvent5.jpg",
-      date: "2019-02-21",
+      date: "2019-04-21",
       visible: true,
       desc:
         "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, excepturi?",
@@ -54,7 +54,7 @@ const axenda = {
     event6: {
       name: "cultureEvento 6",
       image: "/images/cultureEvent6.jpg",
-      date: "2019-03-26",
+      date: "2019-04-26",
       visible: true,
       desc:
         "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, excepturi?",
--- src/sample-Convocatorias.js
+++ src/sample-Convocatorias.js
@@ -17,7 +17,7 @@ const convocatorias = {
     event2: {
       name: "notificationsEvento 2",
       image: "/images/avisosEvent2.jpg",
-      date: "2019-02-22",
+      date: "2019-03-22",
       visible: true,
       desc:
         "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, excepturi?",
@@ -30,7 +30,7 @@ const convocatorias = {
     event3: {
       name: "notificationsEvento 3",
       image: "/images/avisosEvent3.jpg",
-      date: "2019-02-29",
+      date: "2019-03-29",
       visible: true,
       desc:
         "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, excepturi?",
@@ -43,7 +43,7 @@ const convocatorias = {
     event4: {
       name: "notificationsEvento 4",
       image: "/images/avisosEvent4.jpg",
-      date: "2019-03-30",
+      date: "2019-04-30",
       visible: true,
       desc:
         "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, excepturi?",
@@ -56,7 +56,7 @@ const convocatorias = {
     event5: {
       name: "notificationsEvento 5",
       image: "/images/avisosEvent5.jpg",
-      date: "2019-03-31",
+      date: "2019-04-30",
       visible: true,
       desc:
         "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, excepturi?",
@@ -69,7 +69,7 @@ const convocatorias = {
     event6: {
       name: "notificationsEvento 6",
       image: "/images/avisosEvent6.jpg",
-      date: "2019-03-26",
+      date: "2019-04-26",
       visible: true,
       desc:
         "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, excepturi?",
