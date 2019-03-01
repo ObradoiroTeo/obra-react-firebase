58dbbe90e4a159d46d9ea20c86f83e025db8a76a
Sara Miragaya Rico <saramiragaya@gmail.com>
2019-03-01 14:34:54 +0100

    Arreglar

--- src/components/EventoAxenda.js
+++ src/components/EventoAxenda.js
@@ -2,21 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 import { navigate } from "@reach/router";
 
-<<<<<<< HEAD
-const Evento = props => (
-  <button className="event-reduced">
-    <img className="event-image" src={props.eventDetails.image} alt="imagen" />
-    <p className="event-date">
-      {props.eventDetails.date_event.substring(0, 10)}
-    </p>
-    <p className="event-name">{props.eventDetails.name}</p>
-    <p
-      className="desc"
-      dangerouslySetInnerHTML={{ __html: props.eventDetails.desc }}
-    />
-  </button>
-);
-=======
 class EventoAxenda extends React.Component {
   static propTypes = {
     eventDetails: PropTypes.shape({
@@ -29,7 +14,6 @@ class EventoAxenda extends React.Component {
   goToEventDetails = eventKey => {
     navigate(`/axenda/${this.props.eventDetails.name}`);
   };
->>>>>>> 495cbe80b85f6b5ed00dfa484509d3a8cecbe72e
 
   render() {
     return (
