52e8e92d35b31293185e20b76c8415575049e7cd
Sara Miragaya Rico <saramiragaya@gmail.com>
2019-03-06 11:49:25 +0100

    Documentado EventDetailsNovas (Revisar)

--- src/components/EventDetailsNovas.js
+++ src/components/EventDetailsNovas.js
@@ -3,15 +3,33 @@ import PropTypes from "prop-types";
 import Header from "./Header";
 import Footer from "./Footer";
 
-const EventDetails = props => (
+/* Este componente es el que lleva a cada evento de novas individualmente, una vez clicas en él. Muestra el nombre (name), la fecha (date), la imagen (image) y la descripción (desc)*/
+
+const EventDetailsNovas = props => (
   <div>
     <Header />
     <div className="event-container">
-      <p className="details-name">{props.eventDetails.name}</p>
-      <p className="details-date">{props.eventDetails.date}</p>
+      <p className="details-name">
+        {props.eventDetails.events[props.location.state.clave].name}
+      </p>
+      <p className="details-date">
+        {props.eventDetails.events[props.location.state.clave].date.substring(
+          0,
+          10
+        )}
+      </p>
       <div className="details-container">
-        <img src={props.eventDetails.image} alt="img" className="details-img" />
-        <p className="details-desc">{props.eventDetails.desc}</p>
+        <img
+          src={props.eventDetails.events[props.location.state.clave].image}
+          alt="img"
+          className="details-img"
+        />
+        <p
+          className="details-desc"
+          dangerouslySetInnerHTML={{
+            __html: props.eventDetails.events[props.location.state.clave].desc
+          }}
+        />
       </div>
     </div>
     <div className="empty-hack" />
@@ -20,12 +38,12 @@ const EventDetails = props => (
   </div>
 );
 
-EventDetails.propTypes = {
-  eventDetailsNovas: PropTypes.shape({
+EventDetailsNovas.propTypes = {
+  eventDetails: PropTypes.shape({
     name: PropTypes.string.isRequired,
     date: PropTypes.string.isRequired,
     desc: PropTypes.string.isRequired
   }).isRequired
 };
 
-export default EventDetails;
+export default EventDetailsNovas;
