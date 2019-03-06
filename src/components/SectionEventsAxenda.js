bd96e74ce77009c20b0f31a01670b3d8bf262771
Iago C <iagocrpi@gmail.com>
2019-03-06 13:46:08 +0100

    Documentacion de SectionEventsAxenda

--- src/components/SectionEventsAxenda.js
+++ src/components/SectionEventsAxenda.js
@@ -2,6 +2,10 @@ import React from "react";
 import EventoAxenda from "./EventoAxenda";
 import PropTypes from "prop-types";
 
+// En esta funcion hacemos una serie de filtrados para que se muestren unos eventos y otros no en la pagina de Axenda. 👇
+// Primero hacemos un filtrado por la fecha usando (actualDate). Definida y explicada en App.
+// Despues hacemos otro filtrado segun el valor de (visible).
+// Por ultimo usamos un (.map) para aplicar las funciones anteriores a todos los eventos que entran en esta pagina.
 const SectionEventsAxenda = props => (
   <ul className="events-container">
     {Object.keys(props.events)
