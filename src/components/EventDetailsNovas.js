3a58f7ae4237515fb7112a31c761ac8f11a824aa
Diego Sánchez <diegoslores@gmail.com>
2019-03-06 13:57:03 +0100

    Comentarios descriptivo de cada componente.

--- src/components/EventDetailsNovas.js
+++ src/components/EventDetailsNovas.js
@@ -1,3 +1,7 @@
+/**
+ * Componente último que muestra detalladamente y de forma descriptiva cada uno de los eventos.
+ *  Al pinchar en el evento cambia la sección eventos por la descripción del evento en sí.
+ */
 import React from "react";
 import PropTypes from "prop-types";
 import Header from "./Header";
