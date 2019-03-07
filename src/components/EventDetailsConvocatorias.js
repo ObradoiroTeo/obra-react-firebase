60cde2c2d72b5ea777a02d7bc69bb45e046bd57a
Carlota Fernández <carlotaverdejofernandez@gmail.com>
2019-03-07 13:58:07 +0100

    add ApacheDeploy.md,ManejoPdf and changes in EventDetailsConvocatorias.Pdf dentro de Convocatorias

--- src/components/EventDetailsConvocatorias.js
+++ src/components/EventDetailsConvocatorias.js
@@ -7,45 +7,70 @@ import React from "react";
 import PropTypes from "prop-types";
 import Header from "./Header";
 import Footer from "./Footer";
+import ManejoPdf from "./ManejoPdf";
 
 /*   Este componente muestra el nombre (name), la fecha (date_fin_convocatoria), la imagen (image) y la descripción (desc) de cada uno de los eventos*/
-const EventDetailsConvocatorias = props => (
-  <div>
-    <Header />
-    <div className="event-container">
-      <p className="details-name">
-        {props.eventDetails.events[props.location.state.clave].name}
-      </p>
-      <p className="details-date">
-        {props.eventDetails.events[
-          props.location.state.clave
-        ].date_fin_convocatoria.substring(0, 10)}
-      </p>
-      <div className="details-container">
-        <img
-          src={props.eventDetails.events[props.location.state.clave].image}
-          alt="img"
-          className="details-img"
-        />
-        <p
-          className="details-desc"
-          dangerouslySetInnerHTML={{
-            __html: props.eventDetails.events[props.location.state.clave].desc
-          }}
-        />
+class EventDetailsConvocatorias extends React.Component {
+  static propTypes = {
+    eventDetails: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      date: PropTypes.string.isRequired,
+      desc: PropTypes.string.isRequired
+    }).isRequired
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      pdfs: this.props.eventDetails.pdfs
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className="event-container">
+          <p className="details-name">
+            {
+              this.props.eventDetails.events[this.props.location.state.clave]
+                .name
+            }
+          </p>
+          <p className="details-date">
+            {this.props.eventDetails.events[
+              this.props.location.state.clave
+            ].date_fin_convocatoria.substring(0, 10)}
+          </p>
+          <div className="details-container">
+            <img
+              src={
+                this.props.eventDetails.events[this.props.location.state.clave]
+                  .image
+              }
+              alt="img"
+              className="details-img"
+            />
+            <p
+              className="details-desc"
+              dangerouslySetInnerHTML={{
+                __html: this.props.eventDetails.events[
+                  this.props.location.state.clave
+                ].desc
+              }}
+            />
+          </div>
+          <p className="lista-pdf">
+            {this.props.eventDetails.events[
+              this.props.location.state.clave
+            ].pdfs.map(item => (
+              <ManejoPdf item={item} />
+            ))}
+          </p>
+        </div>
+        <div className="empty-hack" />
+        <Footer />
       </div>
-    </div>
-    <div className="empty-hack" />
-    <Footer />
-  </div>
-);
-
-EventDetailsConvocatorias.propTypes = {
-  EventDetailsConvocatorias: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    date: PropTypes.string.isRequired,
-    desc: PropTypes.string.isRequired
-  }).isRequired
-};
+    );
+  }
+}
 
 export default EventDetailsConvocatorias;
