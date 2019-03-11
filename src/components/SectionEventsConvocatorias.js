e084061bf9d8358313b584c217202d8344211ee1
Sara Rodríguez Moldes <raheld12@gmail.com>
2019-03-11 10:34:48 +0100

    Fixed <Convocatorias> CSS

--- src/components/SectionEventsConvocatorias.js
+++ src/components/SectionEventsConvocatorias.js
@@ -12,7 +12,7 @@ import PropTypes from "prop-types";
 // Despues hacemos otro filtrado segun el valor de (visible).
 // Por ultimo usamos un (.map) para aplicar las funciones anteriores a todos los eventos que entran en esta pagina.
 const SectionEventsConvocatorias = props => (
-  <ul className="events-container">
+  <ul className="convocatoria-container">
     {Object.keys(props.events)
       .filter(eventKey => {
         const actualDate = new Date();
