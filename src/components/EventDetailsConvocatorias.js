52e9c77967c52716913ccbe47fe95d0d0a6e3dd2
iagocrespo <44493921+iagocrespo@users.noreply.github.com>
2019-03-21 10:01:23 +0100

    Borrado el Footer (#89)
    * Unfrak
    
    * Podemos ver los pdfs denntro de la pagina de convocatorias
    
    * Ahora podemos ver la descripcion de los eventos de <Axenda> <Convocatorias> y <Novas>
    
    * Borrado <Evento> y creado <EventoNovas>
    
    * Entra en los detalles de los eventos
    
    * intento de arreglo
    
    * modificado Header
    
    * Borrado Footer en todas las paginas y el propio componente
    
    * Modificado el link de donde obtenemos los .json

--- src/components/EventDetailsConvocatorias.js
+++ src/components/EventDetailsConvocatorias.js
@@ -3,7 +3,6 @@
  */
 import React from "react";
 import Header from "./Header";
-import Footer from "./Footer";
 import ManejoPdf from "./ManejoPdf";
 
 class EventDetailsConvocatorias extends React.Component {
@@ -55,7 +54,6 @@ class EventDetailsConvocatorias extends React.Component {
           </div>
         </div>
         <div className="empty-hack" />
-        <Footer />
       </div>
     );
   }
