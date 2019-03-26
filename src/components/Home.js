ea90d05f89649b846e2a40a6e16999aa1e72808c
Diegosl <44493933+diegoslores@users.noreply.github.com>
2019-03-26 09:09:26 +0100

    Pull request/logotipo (#100)
    * Cambios en .json
    
    * Conexion a firebase y componentes externos.
    
    * Cambios para que funcione npm install
    
    * modificaciones para  que funcionen los state de forma externa
    
    * merge de json
    
    * Unfrak
    
    * Merge
    
    * wip
    
    * Guia para deploy en Apache.
    
    * guia apache WIP
    
    * Merge OriginMaster
    
    * json
    
    * Podemos ver los pdfs denntro de la pagina de convocatorias
    
    * WIP
    
    * Ahora podemos ver la descripcion de los eventos de <Axenda> <Convocatorias> y <Novas>
    
    * Borrado <Evento> y creado <EventoNovas>
    
    * Entra en los detalles de los eventos
    
    * intento de arreglo
    
    * pruebas
    
    * Comentario en APP
    
    * Comentarios descriptivo de cada componente.
    
    * NGINX deploy guide
    
    * pull-request/nginx-deploy
    
    * WIP
    
    * Borrado de carpeta Fuente
    
    * WIP
    
    * pruebas css
    
    * Cambios en CSS
    
    * WIP
    
    * Fusión definitiva
    
    * Definitivo?
    
    * box-sizing en style de  iframes
    
    * Cambios en informaTeo css/js
    
    * borrado de archivos innecesarios
    
    * logotipo obradoiro inicio

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,18 +9,32 @@ class Home extends React.Component {
           <Header />
         </div>
         <div className="texto-inicio">
-          Benvidos á nova aplicación do Concello de Teo, onde se poderá
-          consultar a axenda do concello cas súas respectivas novas, así como as
-          distintas actividades oficiais para informarse de prazos, solicitudes
-          e demais.
-          <br />
-          <br />
-          Se te perdiches algún número da nosa revista mensual InformaTeo, aquí
-          poderase botar unha ollada.
-          <br />
-          <br />
-          Dende á aplicación tamén podese acceder á paxina web oficial do
-          Concello de Teo e tamén á de SomosTeo.
+          {/*<img
+            src="/images/logoObradoiroGrande.jpg"
+            className="logo-obradoiro"
+            alt="logo-obradoiro"
+          />*/}
+          <p className="logo-titulo">
+            Obradoiro Emprego Habilits
+            <br />
+            Desenvolvemento de Aplicacións con Tecnoloxía Web
+          </p>
+          <p className="texto-titulo">
+            <br />
+            <br />
+            Benvidos á nova aplicación do Concello de Teo, onde se poderá
+            consultar a axenda do concello cas súas respectivas novas, así como
+            as distintas actividades oficiais para informarse de prazos,
+            solicitudes e demais.
+            <br />
+            <br />
+            Se te perdiches algún número da nosa revista mensual InformaTeo,
+            aquí poderase botar unha ollada.
+            <br />
+            <br />
+            Dende á aplicación tamén podese acceder á paxina web oficial do
+            Concello de Teo e tamén á de SomosTeo.
+          </p>
         </div>
       </>
     );
